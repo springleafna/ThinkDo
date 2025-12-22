f771a3db388a2769dcf7a6b0851837a630c825cc
springleaf <3313252209@qq.com>
2025-12-22 23:40:05 +0800

    feat: 项目结构搭建

---
+++ think-do-start/src/main/java/com/springleaf/thinkdo/ThinkDoApplication.java
@@ -0,0 +1,11 @@
+package com.springleaf.thinkdo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinkDoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ThinkDoApplication.class, args);
+    }
+}
